27c9ed46242c152f2a80ed0d90576054e190c7d9
fchoi1 <masterfabster2010@hotmail.com>
2022-02-19 23:24:57 -0500

    inital commit and structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// Connect mongoose to server
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
